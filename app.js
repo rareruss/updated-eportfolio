e7d171032643b1b14231ceb08b980f5e1e8b8dbe
Russell <ruselllouviere5315@gmail.com>
2024-08-04 20:10:34 -0500

    first commit of updated eportfolio

---
+++ app.js
@@ -0,0 +1,56 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+   const cigars = document.querySelectorAll(".cigar");
+   const x = event.clientX * scaleFactor;
+   const y = event.clientY * scaleFactor;
+   
+   for (let i = 0; i < cigars.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    cigars[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+   }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact (event) {
+event.preventDefault();
+const loading = document.querySelector('.modal__overlay--loading')
+const success = document.querySelector('.modal__overlay--success')
+loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm (
+            'service_0y9g82p',
+            'template_2mpz0ch',
+            event.target,
+            '7W-F3oUyH_a0G2kMg'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on russelllouviere00@gmail.com"
+            ) 
+        })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal__open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal__open"
+}
